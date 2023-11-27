01ab6929f5483dfc311236291b7700bd69e4dec7
Tim1284 <timea.csomor@icloud.com>
2023-11-27 17:18:12 +0000

    Comments added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//DOM elements
 const questionsNumberInput = document.querySelector("#questions-amount");
 const categoryInput = document.querySelector("#category");
 const levelInput = document.querySelector("#level");
@@ -12,17 +13,20 @@ const answersContainer = document.querySelector(".answers-container");
 const errorMessage = document.querySelector(".error");
 const DISPLAY_DELAY = 1000;
 
+//Game state variables
 let questions = [];
 let timer,
   time,
   currQuestion,
   score = 0;
 
+//Function to display progress in the progress bar
 const progress = (val) => {
   const percent = (val / time) * 100;
   progressBar.style.width = `${percent}%`;
 };
 
+//Function to launch the trivia game
 async function triviaLaunch() {
   try {
     const number = questionsNumberInput.value;
@@ -30,6 +34,7 @@ async function triviaLaunch() {
     const level = levelInput.value;
     loading();
 
+    //Fetch questions from the Open Trivia Database
     const url = `https://opentdb.com/api.php?amount=${number}&category=${category}&difficulty=${level}&type=multiple`;
 
     const response = await fetch(url);
@@ -46,8 +51,10 @@ async function triviaLaunch() {
       );
     }
 
+    //Store fetched questions
     questions = results.results;
 
+    //Transition to the game display after a delay
     setTimeout(() => {
       startDisplay.classList.add("hide");
       gameDisplay.classList.remove("hide");
@@ -58,11 +65,13 @@ async function triviaLaunch() {
   } catch (error) {
     console.error("An error occurred:", error.message);
 
+    //Display error message if an error occurs during launch
     errorMessage.classList.remove("hide");
     errorMessage.innerHTML = `Something went wrong!!! ${error.message}`;
   }
 }
 
+//Function to display a question
 const displayQuestion = (question) => {
   updateQuestionText(question);
   displayAnswers(question);
@@ -71,12 +80,14 @@ const displayQuestion = (question) => {
   launchTimer(time);
 };
 
+//Function to update the text of the current question
 const updateQuestionText = (question) => {
   const questionText = document.querySelector(".question-text");
   questionText.innerHTML = `
     ${currQuestion} / ${questions.length} ${question.question}`;
 };
 
+//Function to display answer choices
 const displayAnswers = (question) => {
   const answers = [
     ...question.incorrect_answers,
@@ -92,6 +103,7 @@ const displayAnswers = (question) => {
   addAnswerEventListeners();
 };
 
+//Function to create an HTML element for an answer choice
 const createAnswerElement = (answer) => {
   return `
       <div class="answer">
@@ -103,6 +115,7 @@ const createAnswerElement = (answer) => {
       `;
 };
 
+//Function to add event listeners to answer choices
 const addAnswerEventListeners = () => {
   const answerWrap = document.querySelectorAll(".answer");
   answerWrap.forEach((answer) => {
@@ -112,6 +125,7 @@ const addAnswerEventListeners = () => {
   });
 };
 
+//Function to handle a click on an answer choice
 const handleAnswerClick = (answerWrap, answer) => {
   if (!answer.classList.contains("checked")) {
     answerWrap.forEach((answer) => {
@@ -123,6 +137,7 @@ const handleAnswerClick = (answerWrap, answer) => {
   }
 };
 
+//Function to launch the timer for each question
 const launchTimer = (time) => {
   timer = setInterval(() => {
     if (time >= 0) {
@@ -134,6 +149,7 @@ const launchTimer = (time) => {
   }, DISPLAY_DELAY);
 };
 
+//Function to display loading animation
 const loading = () => {
   startButton.textContent = ".";
   const loadingInterval = setInterval(() => {
@@ -148,9 +164,11 @@ const loading = () => {
   }, DISPLAY_DELAY);
 };
 
+//Button elements
 const btnSubmit = document.querySelector(".submit");
 const btnNext = document.querySelector(".next");
 
+//Event listeners for submit and next buttons
 btnSubmit.addEventListener("click", () => {
   checkAnswer();
 });
@@ -161,6 +179,7 @@ btnNext.addEventListener("click", () => {
   btnNext.style.display = "none";
 });
 
+//Function to check the selected answer and update the score
 const checkAnswer = () => {
   clearInterval(timer);
   const clickedAnswer = document.querySelector(".answer.selected");
@@ -202,6 +221,7 @@ const checkAnswer = () => {
   btnNext.style.display = "flex";
 };
 
+//Function to move to the next question or display the final score
 const nextQuestion = () => {
   if (currQuestion < questions.length) {
     currQuestion++;
@@ -211,20 +231,25 @@ const nextQuestion = () => {
   }
 };
 
+//Results display elements
 const resultsDisplayEl = document.querySelector(".results-display");
 const finalScore = document.querySelector(".final-score");
 
+//Function to display the final score
 const displayScore = () => {
   resultsDisplayEl.classList.remove("hide");
   gameDisplay.classList.add("hide");
   finalScore.innerHTML = `You scored ${score} / ${questions.length}`;
 };
 
+//Button element to restart the game
 const btnRestart = document.querySelector(".restart");
 
+//Event listener for the restart button
 btnRestart.addEventListener("click", () => {
   score = 0;
   window.location.reload();
 });
 
+//Event listener for the start button to launch the game
 startButton.addEventListener("click", triviaLaunch);
